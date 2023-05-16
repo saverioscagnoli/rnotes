0f683a132689997622dee10c7503f13925abe1eb
Saverio <svscagn@gmail.com>
2023-05-16 21:36:17 +0200

    navbar

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,8 +1,9 @@
-import { Editor } from "./components";
+import { Editor, Navbar } from "./components";
 
 function App(): JSX.Element {
   return (
     <div style={{ width: "100vw", height: "100vh" }}>
+      <Navbar />
       <Editor />
     </div>
   );
