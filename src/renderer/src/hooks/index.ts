514cf6e2ae6888977747379561a1a943c9ab80b5
Saverio <svscagn@gmail.com>
2023-05-16 20:50:58 +0200

    auto closing

--- src/renderer/src/hooks/index.ts
+++ src/renderer/src/hooks/index.ts
@@ -1,2 +1,1 @@
 export { default as useEditor } from "./useEditor";
-export { default as useAutoClosing } from "./useAutoClosing";
--- src/renderer/src/hooks/useEditor.ts
+++ src/renderer/src/hooks/useEditor.ts
@@ -1,20 +1,59 @@
-import { ChangeEvent, useState } from "react";
-import useAutoClosing from "./useAutoClosing";
+import { useState, useRef, useEffect } from "react";
+
+const close = {
+  "(": ")",
+  "[": "]",
+  "{": "}",
+  '"': '"',
+  "'": "'",
+  "*": "*",
+  $: "$"
+};
 
 const useEditor = () => {
   const [val, setVal] = useState<string>("");
+  const txRef = useRef<HTMLTextAreaElement>(null);
 
-  const onEdit = (e: ChangeEvent<HTMLTextAreaElement>) => {
+  const onEdit = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     let v = e.target.value;
-    let closing = useAutoClosing(v.slice(-1));
-    if (closing.length === 1) {
-      setVal(v + closing);
-    } else if (closing.length > 1) {
-      setVal(closing);
-    } else setVal(v);
+    setVal(v);
   };
 
-  return { val, onEdit };
+  const onKeyDown = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    if (e.key in close) {
+      e.preventDefault();
+
+      const tx = txRef.current;
+      if (!tx) return;
+      const [stP, endP] = [tx.selectionStart, tx.selectionEnd];
+
+      const slTxt = val.substring(stP, endP);
+      const toClose = close[e.key];
+
+      let updVal: string;
+      let updPos: number;
+
+      if (slTxt) {
+        updVal =
+          val.substring(0, stP) + e.key + slTxt + toClose + val.substring(endP);
+        updPos = stP + slTxt.length + 2;
+      } else {
+        updVal = val.substring(0, stP) + e.key + toClose + val.substring(endP);
+        updPos = stP + 1;
+      }
+      setVal(updVal);
+      setTimeout(() => tx.setSelectionRange(updPos, updPos));
+    }
+  };
+
+  useEffect(() => {
+    const tx = txRef.current;
+    if (tx) {
+      tx.focus();
+    }
+  }, []);
+
+  return { val, onEdit, onKeyDown, txRef };
 };
 
 export default useEditor;
